3725ae4a80bdfa1a67adad297e0f47f1402fac32
tuanfaris <muhdfaris9494@gmail.com>
2024-08-23 09:25:52 +0800

    Update game.js

--- game.js
+++ game.js
@@ -9,7 +9,7 @@ const gameBoard = document.getElementById('game-board')
 function main(currentTime) {
   if (gameOver) {
     if (confirm('You lost. Press ok to restart.')) {
-      window.location = '/'
+      window.location = 'https://github.com/tuanfaris/snake/blob/main/game.js'
     }
     return
   }
